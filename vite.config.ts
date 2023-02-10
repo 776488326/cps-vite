f2eceb189b9a1c7d0741593ad00aa0dbde5c3909
冯亮 <fengliang@gridsum.com>
2023-02-10 09:34:13 +0800

    config: 修改vite配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 代理地址
+const PROXY_TARGET = "http://test.cps.gridsum.com:38083";
+
+// 代理端口
+const PORT = 3210;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      "/api": {
+        target: PROXY_TARGET,
+        changeOrigin: true,
+      },
+      "/api/ws": {
+        target: PROXY_TARGET,
+        changeOrigin: true,
+        ws: true
+      }
+    },
+    port: PORT,
+    open: true,
+    cors: true,
+    
+  }
+
 })
